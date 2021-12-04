caa14df6ebaee5bf7e387e0ba9bfa89e2c7b079c
Hironori Sakai <crescent.lab@gmail.com>
2021-12-05 00:55:47 +0100

    use pytest instead of unittest

--- test/emo_classifier/test_artifact.py
+++ test/emo_classifier/test_artifact.py
@@ -1,33 +1,27 @@
-from unittest import TestCase
 from dataclasses import dataclass
 
 from emo_classifier.artifact import JsonArtifact
 from lib import PROJ_ROOT
+
+dummy_name = "dummy_name"
+dummy_value = 1234
+file_path = PROJ_ROOT / "emo_classifier/data/DummyJsonArtifact.json"
+
+if file_path.exists():
+    file_path.unlink(missing_ok=True)
 
 @dataclass
 class DummyJsonArtifact(JsonArtifact):
     name: str
     value: int
 
-class TestJsonArtifact(TestCase):
-    dummy_name = "dummy_name"
-    dummy_value = 1234
-    file_path = PROJ_ROOT / "emo_classifier/data/DummyJsonArtifact.json"
+def test_save_and_load():
+    dummy_json_artifact = DummyJsonArtifact(dummy_name, dummy_value)
+    dummy_json_artifact.save()
+    assert file_path.exists()
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        if cls.file_path.exists():
-            cls.file_path.unlink(missing_ok=True)
+    loaded_artifact = DummyJsonArtifact.load()
+    assert isinstance(loaded_artifact, DummyJsonArtifact)
+    assert dummy_json_artifact == loaded_artifact
 
-    def test_save_and_load(self):
-        dummy_json_artifact = DummyJsonArtifact(self.dummy_name, self.dummy_value)
-        dummy_json_artifact.save()
-        self.assertTrue(self.file_path.exists())
-
-        loaded_artifact = DummyJsonArtifact.load()
-        self.assertIsInstance(loaded_artifact, DummyJsonArtifact)
-        self.assertEqual(dummy_json_artifact, loaded_artifact)
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.file_path.unlink(missing_ok=True)
+    file_path.unlink(missing_ok=True)
